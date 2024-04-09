792b2d73c658908b32c170ca78d431bf3d9a24a5
João Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-04-09 14:18:40 -0300

    adicionando aula 8 ]

---
+++ Aula 8/aula8b.py
@@ -0,0 +1,6 @@
+from math import sqrt, floor
+num = int(input('Digite um úmero: '))
+raiz = sqrt(num)
+print('A raiz de {} é {}!'.format(num, floor(raiz)))
+
+#Floor serve para arredondar para baixo e sqrt é para fazer a raiz quadrada do número.
--- Aula7/aula7a.py
+++ Aula7/aula7a.py
@@ -1,3 +1,4 @@
 nome = input('qual o seu nome?')
 print('Prazer em te conhecer, {:^20}!'.format(nome))
+print('oii')
 
