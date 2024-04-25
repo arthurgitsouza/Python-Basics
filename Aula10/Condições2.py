19a295a517e45589601dff14b1e803d8d410c428
João Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-04-25 13:05:59 -0300

    p1

---
+++ Aula10/Condições2.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual é o seu nome? ')).strip()
+if nome == 'Arthur':
+    print('Que nome lindo você tem!!')
+else:
+    print('Seu nome não é honroso para a espada Escalibur.')
+print('Foi um prazer {}!'.format(nome))
