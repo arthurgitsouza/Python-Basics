bdb23c6c620495cdda7803e9aa852ade2a8164f4
João Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-04-26 17:59:26 -0300

    p1

---
+++ Aula11/Cores2.py
@@ -0,0 +1,3 @@
+nome = str(input('Qual o seu nome? '))
+print('Olá, é um prazer conhecê-lo, {}{}{}\033[34m!!!'.format('\033[33m',nome,'\033[m'))
+#Forma de adicionar cor no terminal através de um format!
---
+++ Aula11/Cores_no_Py.py
@@ -0,0 +1,6 @@
+a = 5
+b = 3
+print('Os valores são \033[0;32m{}\033[m e \033[35m{}!'.format(a, b))
+
+# O código \033[m é utilizado para adicionar cores ao terminal do python
+# Com ele é possivel alterar o Style, o Text e o Background.
