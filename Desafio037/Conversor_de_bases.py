1acf8ae1836dafc719f19087f230ba26292d740e
João Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-05-13 14:13:55 -0300

    p1

--- Desafio037/Conversor_de_bases.py
+++ Desafio037/Conversor_de_bases.py
@@ -4,4 +4,12 @@ cores = {'limpa' : '\033[m',
          'vermelho' : '\033[31m',
          'rosa'  : '\033[35m'}
 print('{}Escolha uma dessas opções: {}\n[1] converter para BINÁRIO \n[2] converter para OCTAL \n[3] converter para HEXADECIMAL{}'.format(cores['vermelho'],cores['roxo'], cores['limpa']))
-op = int(input('{}Sua opção:'.format(cores['rosa'])))
+op = int(input('{}Sua opção:{}'.format(cores['rosa'], cores['limpa'])))
+if op == 1:
+    print('O número {} convertido para BINÁRIO é igual a {}.'.format(num, bin(num)[2:]))
+elif op == 2:
+    print('O número {} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+elif op == 3:
+    print('O número {} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção inválida, tente novamente!') 
