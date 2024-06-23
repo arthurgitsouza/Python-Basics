e88aa3e1d43784df2399f960eb3cb83990dadfa4
User <arthurgitsouz@gmail.com>
2024-06-23 12:24:02 -0300

    p1

---
+++ Desafio055/Maior_e_menor_da_sequencia.py
@@ -0,0 +1,15 @@
+maiorpeso = 0
+menorpeso = 0
+for pess in range(1, 6):
+    p = float(input('Peso da {}ª pessoa: '.format(pess)))
+    if p == 1:
+        maiorpeso = pess
+        menorpeso = pess
+    else:
+        if p > maiorpeso:
+            maiorpeso = p
+        if p < menorpeso:
+            menor = p
+print('O maior peso lido foi de {}Kg'.format(maiorpeso))
+print('O menor peso lido foi de {}Kg'.format(menorpeso))
+ 
