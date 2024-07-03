6b96d321d373e9e4cf4c7d46a7c26376816552bf
Joao Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-07-03 14:28:56 -0300

    p1

--- Desafio059/Progressão_aritmética_v2.0.py
+++ Desafio059/Progressão_aritmética_v2.0.py
@@ -1,2 +1,11 @@
-print(''*4'Gerador de PA'''*4)
+print('Gerador de PA')
 print('-='*10)
+pt = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = pt
+cont = 1
+while cont <= 10:
+    print('{}'.format(termo), end = '')
+    termo += razao #termo = termo + razao
+    cont =+ 1
+print('FIM!')
