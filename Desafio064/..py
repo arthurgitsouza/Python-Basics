960dedbc95b3fecdd7da79f0ab3079d4ed077ebb
Joao Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-07-04 15:05:00 -0300

    p1

---
+++ Desafio064/..py
@@ -0,0 +1,10 @@
+print('-'*23)
+print('Sequencia de Fibonacci!')
+print('-'*23)
+termos = int(input('Quantos termos você quer mostrar? '))
+print('^~^'*23)
+termoum = 0
+termodois = 1
+termotres = termoum + termodois
+while termoum != termotres*10:
+    print(termoum+termotres)
