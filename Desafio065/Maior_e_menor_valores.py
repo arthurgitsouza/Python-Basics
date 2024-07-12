dc9b8edc7f87986817d9b36efe9cdef067b560eb
Joao Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-07-12 12:15:31 -0300

    p1

--- Desafio065/Maior_e_menor_valores.py
+++ Desafio065/Maior_e_menor_valores.py
@@ -1,9 +1,17 @@
 pergunta = 'S'
-soma = cont = media = 0
-while pergunta in 'S':
+soma = cont = media = menor = maior = 0
+while pergunta == 'S':
     n = int(input('Digite um número: '))
     soma += n
     cont += 1 
+    if cont == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
     pergunta = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
-    media = soma/cont
-print(f'Você digitou {cont} números e a média entre eles é: {media:.2f}')
+media = soma / cont
+print(f'Você digitou {cont} números e a média entre eles é: {media:.1f}!')
+print(f'O maior valor foi{maior} e o menor valor foi {menor}!')
