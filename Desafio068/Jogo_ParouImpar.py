04c7c3d75dfe1fcbc41baa5d106ef68e320da718
Joao Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-07-23 15:39:50 -0300

    p1

--- Desafio068/Jogo_ParouImpar.py
+++ Desafio068/Jogo_ParouImpar.py
@@ -4,17 +4,18 @@ print('VAMOS JOGAR PAR OU IMPAR!')
 print('-'*30)
 computador = randint(0,10)
 cont = 0
-while True:
+print(computador)
+'''while True:
     jogador = int(input('Diga um valor: '))
     decisao = str(input('Par ou Impar? [P/I]')).strip().upper()
     total = jogador + computador
     print(f'Você jogou {jogador} e o computador {computador}. Total de {total}!')
     if decisao == 'P' and total % 2 == 0:
-        print('E {total} é Par, então...')
+        print(f'{total} é Par, então...')
         print('Você VENCEU!! \nMas... Vamos jogar novamente...')
         cont +=1
     else:
         print(f'{total} é impar, então...')
         print('Você PERDEU!')
         print(f'GAME OVER! e Você venceu apenas {cont} vezes.')
-        break
+        break'''
--- Desafio071/Caixa_eletrônico.py
+++ Desafio071/Caixa_eletrônico.py
@@ -1,8 +1,28 @@
 print('='*40)
 print(f'{'BANCO CEV':-^40}')
 print('='*40)
-valor = float(input('Que valor você quer casar? R$'))
-cont = 0
-while valor % 50 == 0:
-    cont += 1
-print(cont)
+valor = float(input('Que valor você quer sacar? R$'))
+cont50 = cont20 = cont10 = cont1 = 0
+while valor > 50:
+    valor = valor - 50
+    cont50 += 1
+    while valor < 50 and valor >= 20:
+        valor = valor - 20
+        cont20 += 1
+    while valor < 20 and valor >= 10:
+        valor = valor - 10
+        cont10 += 1
+    while valor < 10 and valor > 0:
+        valor = valor - 1
+        cont1 += 1
+    if valor == 0:
+        break
+if cont50 > 0:
+        print(f'Total de {cont50} células de R$50,00.')
+if cont20 > 0:
+    print(f'Total de {cont20} células de R$20,00.')
+if cont10 > 0:
+    print(f'Total de {cont10} células de R$10,00.')
+if cont1 > 0:
+    print(f'Total de {cont1} células de R$1,00.')
+print(f'{'Volte sempre ao BANCO CEV, Tenha um bom dia!':-^40}')
