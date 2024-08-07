b42eb7bbf01275e66863c94aeffaa5de3b194c57
User <arthurgitsouz@gmail.com>
2024-08-07 16:35:01 -0300

    p1

---
+++ Desafio095/Aprimorando_dicionários.py
@@ -0,0 +1,44 @@
+time = []
+jogador = {}
+partidas = []
+tot = 0
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador['nome']} jogou?'))
+    partidas.clear()
+    for p in range(0,tot):
+        partidas.append(int(input(f'Quantos gols na partida {p}? ')))
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        op = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+        if op in 'SN':
+            break
+        print('Erro! Responda apenas S ou N.')
+    if op == 'N':
+            break
+print('-='*30)
+print('cod', end = '')
+for i in jogador.keys():
+     print(f'{i:<15}', end = '')
+print()
+print('-='*30)
+for k, v in enumerate(time):
+    print(f'{k:>3}', end = '')
+    for d in v.values():
+        print(f'{str(d):<13}', end = '')
+    print()
+print('-='*30)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar)'))
+    if busca == 999:
+         break
+    if busca >= len(time):
+         print(f'ERRO! Não existe jogador com código {busca}')
+    else:
+         print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
+         for i, g in enumerate(time['gols']):
+              print(f'      No jogo {i+1} fez {g} gols.')
+    print('-='*30)
+print('VOLTE SEMPRE!')
